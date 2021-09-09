276617c821b639424ce784b1c1ab604c458bf2c9
Darshan Borase <darshan.borase@successive.tech>
2021-09-09 14:16:41 +0530

    Day 11 Task- minor changes

--- extraTs/constant.ts
+++ extraTs/constant.ts
@@ -1,23 +1,25 @@
+
+
 import { Users, Permission } from './interfaces';
 
 const permission: Permission = {
-    'getUsers': {
-        all: ['head-trainer'],
-        read : ['trainee', 'trainer'],
-        write : ['trainer'],
-        delete: [],
-    },
+'getUsers': {
+all: ['head-trainer'],
+read : ['trainee', 'trainer'],
+write : ['trainer'],
+delete: [],
+},
 };
 
 const users: Users = [
-    {
-        traineeEmail: 'darshan.borase@successive.tech',
-        reviewerEmail: 'shekhar.patil@successive.tech',
-    },
-    {
-        traineeEmail: 'darshan.borase@successvietech',
-        reviewerEmail: 'shekhar.patil@successivetech'
-    }
+{
+traineeEmail: 'darshan.borase@successive.tech',
+reviewerEmail: 'shekhar.patil@successive.tech',
+},
+{
+traineeEmail: 'darshan.borase@successvietech',
+reviewerEmail: 'shekhar.patil@successivetech'
+}
 ];
 
 export default {permission, users};
